0697f936e4573a1b64c0a38ee959a83a4ee70c6f
Raul Andrial <rauljandrial@gmail.com>
2022-10-05 22:13:42 +0000

    Have build error, will commit what I have and create new container

--- src/app.js
+++ src/app.js
@@ -4,8 +4,37 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { getSupportInfo } from "prettier";
+
+// Generates a random whole number between 0 and the number passed through inclusive
+function genRandomNum(maxNum) {
+  return Math.floor(Math.random() * maxNum);
+}
+
+// Generate the suit of the card randomly
+function genSuit() {
+  const suits = ["♦", "♥", "♠", "♣"];
+  return suits[genRandomNum(suits.length)];
+}
+
+// Generate the value of the card randomly
+function genCardValue() {
+  const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  return cards[genRandomNum(cards.length)];
+}
+
+// Generate random card
+function genCard() {
+  return {
+    suit: genSuit(),
+    value: genCardValue()
+  };
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = genCard();
+
+  console.log(card.suit);
+  console.log(card.value);
 };
